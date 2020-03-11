a0e25fe41623bd9feedf091203a2b1d7397ae51e
Seungwook Han <sh3264@coumbia.edu>
2020-03-11 13:36:31 -0400

    Adding logs to model

--- src/train_iwtvae_64.py
+++ src/train_iwtvae_64.py
@@ -33,10 +33,13 @@ if __name__ == "__main__":
 
     if args.mask:
         iwt_model = IWTVAE_64_Mask(z_dim=args.z_dim, upsampling=args.upsampling, num_upsampling=args.num_upsampling, reuse=args.reuse)
+        LOGGER.info('Running mask model')
     elif args.bottleneck > 0:
         iwt_model = IWTVAE_64(z_dim=args.z_dim, bottleneck_dim=args.bottleneck, upsampling='bottleneck', num_upsampling=args.num_upsampling, reuse=args.reuse)
+        LOGGER.info('Running bottleneck model')
     else:
         iwt_model = IWTVAE_64(z_dim=args.z_dim, upsampling=args.upsampling, num_upsampling=args.num_upsampling, reuse=args.reuse)
+        LOGGER.info('Running original model with upsampling = {}'.format(args.upsampling))
 
     iwt_model = iwt_model.to(devices[0])
     iwt_model.set_devices(devices)
