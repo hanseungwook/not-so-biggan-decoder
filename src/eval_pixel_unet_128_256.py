13249ca22f0bf1b06bf8926d04ec3356ac8846dc
Seungwook Han <sh3264@coumbia.edu>
2020-05-29 18:01:13 -0400

    Turn off shuffle

--- src/eval_pixel_unet_128_256.py
+++ src/eval_pixel_unet_128_256.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     valid_dataset = dset.ImageFolder(root=args.valid_dir, transform=default_transform)
 
     valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size,
-                                               shuffle=True, num_workers=args.workers,
+                                               shuffle=False, num_workers=args.workers,
                                                pin_memory=True, drop_last=True)
 
     model_128 = UNet(n_channels=3, n_classes=3, bilinear=True)
