1ea7e64aa6764208fbbcd785cf451e87d203f959
Seungwook Han <sh3264@coumbia.edu>
2020-09-03 14:50:20 +0900

    Fixed constructor

--- src/datasets.py
+++ src/datasets.py
@@ -70,7 +70,7 @@ class LSUN_Objects(VisionDataset):
     """
 
     def __init__(self, root, classes='train', transform=None, target_transform=None):
-        super(LSUN, self).__init__(root, transform=transform,
+        super(LSUN_Objects, self).__init__(root, transform=transform,
                                    target_transform=target_transform)
         self.classes = self._verify_classes(classes)
 
